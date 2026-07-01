239b6955923db2f3eba1ac74940cad8d3625346c
Debasish Pradhan <dp0000000004@gmail.com>
2026-07-01 12:44:49 +0530

    Add __str__ method to model for string representation

--- backend/restorantapp/models.py
+++ backend/restorantapp/models.py
@@ -25,3 +25,6 @@ class Order(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return  self.menu
+
